ad511b399eddb97b5a878996e7485a42e2d55c0d
Alejandro Lara Doña <50618873+ale87jan@users.noreply.github.com>
2024-06-17 12:59:33 +0200

    Update main_test.c

--- main_test.c
+++ main_test.c
@@ -4,7 +4,7 @@ int contar_valor_en_array(int valor,int *array, unsigned int longitud, int *resu
 
 int main(int argc, char *argv[]) {
   int array[10] = {1, 2, 3, 3, 3, 4, 5, 6, 7, 3};
-  int valor = 3;
+  int valor = argc==0? 3: atoi(argv[0]);
   int ret;
   int cuenta;
   ret = contar_valor_en_array(valor, array, 10, &cuenta);
